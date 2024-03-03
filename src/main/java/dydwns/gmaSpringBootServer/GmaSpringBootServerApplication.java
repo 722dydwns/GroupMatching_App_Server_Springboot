2d9e744db30f1b250f2557ed8569ed94c09a033f
YongJOoOo <we072200@naver.com>
2024-03-03 15:40:44 +0900

    #1 first commit

---
+++ src/main/java/dydwns/gmaSpringBootServer/GmaSpringBootServerApplication.java
@@ -0,0 +1,13 @@
+package dydwns.gmaSpringBootServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmaSpringBootServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmaSpringBootServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dydwns/gmaSpringBootServer/GmaSpringBootServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dydwns.gmaSpringBootServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmaSpringBootServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
